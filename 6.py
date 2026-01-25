eda31c52cdd7eef49a550ed88066033dea7838b8
Alina Mukanova <mukanovaa947-code@gmail.com>
2026-01-26 00:04:35 +0500

    Alina

---
+++ 6.py
@@ -0,0 +1,34 @@
+x = 5
+print(type(x))
+
+x = "Hello World"
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = ["apple", "banana", "cherry"]
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = True
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = None
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
---
+++ 9.py
@@ -0,0 +1,21 @@
+print("Hello")
+print('Hello')
+
+b = "Hello, World!"
+print(b[2:])
+
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt) 
+
