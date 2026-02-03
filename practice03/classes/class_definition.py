692d0067e33cb36acd5151795f81dd2e0b368b2d
Alina Mukanova <mukanovaa947-code@gmail.com>
2026-02-03 23:09:09 +0500

    alina

---
+++ practice03/classes/class_definition.py
@@ -0,0 +1,11 @@
+#1
+class MyClass:
+  x = 5
+
+#2
+p1 = MyClass()
+print(p1.x)
+
+#3
+del p1
+
---
+++ practice03/classes/init_method.py
@@ -0,0 +1,32 @@
+#1
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Emil", 36)
+
+print(p1.name)
+print(p1.age)
+
+#2
+class Person:
+  pass
+
+p1 = Person()
+p1.name = "Tobias"
+p1.age = 25
+
+print(p1.name)
+print(p1.age)
+
+#3
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Linus", 28)
+
+print(p1.name)
+print(p1.age)
